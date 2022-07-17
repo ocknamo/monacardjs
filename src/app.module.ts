a38ef264944c6ddf05944ebe21e50710aedc8f62
ocknamo <s14pes@gmail.com>
2022-07-17 23:49:30 +0900

    fix: format

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,8 @@ const configService = new ConfigService();
     CidModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: configService.get<string>('DATABASE_HOST'), port: configService.get<number>('DATABASE_PORT'),
+      host: configService.get<string>('DATABASE_HOST'),
+      port: configService.get<number>('DATABASE_PORT'),
       username: configService.get<string>('DATABASE_USER'),
       password: configService.get<string>('DATABASE_PASSWORD'),
       database: configService.get<string>('DATABASE_NAME'),
