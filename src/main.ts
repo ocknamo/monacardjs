9ffdbc128b5cb94780983ca75203918683abf17b
ocknamo <s14pes@gmail.com>
2022-08-26 23:46:56 +0900

    feat: set trust proxy to true

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
   app.useGlobalPipes(
@@ -14,6 +15,7 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.set('trust proxy', 1);
   await app.listen(3000);
 }
 bootstrap();
