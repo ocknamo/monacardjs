3771251a2ebc0407aac1c39eccf9b0841fe0f954
ocknamo <s14pes@gmail.com>
2022-08-26 23:46:18 +0900

    fix: update log format

--- src/common/http-exception.filter.ts
+++ src/common/http-exception.filter.ts
@@ -9,11 +9,13 @@ import { Response } from 'express';
 import { inspect } from 'util';
 
 export interface ErrorLog {
+  id?: string;
   status?: number;
   message?: string;
   method?: string;
   requestUrl?: string;
   stack?: string;
+  error: string;
 }
 
 @Catch(HttpException)
@@ -26,14 +28,16 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const status = exception.getStatus();
 
     const log: ErrorLog = {
+      id: request['requestId'],
       status: status,
       message: exception.message,
       method: request.method,
       requestUrl: request.url,
       stack: exception.stack,
+      error: inspect(exception, { depth: 4 }),
     };
 
-    this.logger.error(inspect(log));
+    this.logger.error(JSON.stringify(log).replaceAll('\n', ''));
 
     response.status(status).json({
       statusCode: status,
--- src/common/logger.middleware.ts
+++ src/common/logger.middleware.ts
@@ -1,30 +1,39 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { Request, Response, NextFunction } from 'express';
 import { inspect } from 'util';
 
 interface RequestLog {
+  id: string;
   method: string;
   path: string;
   query: string;
   body: string;
   ip: string;
   ips: string[];
+  userAgent?: string;
 }
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(LoggerMiddleware.name);
   use(req: Request, res: Response, next: NextFunction) {
+    if (!req['requestId']) {
+      req['requestId'] = randomUUID();
+    }
+
     const logMessage: RequestLog = {
+      id: req['requestId'],
       method: req.method,
       path: req.path,
-      query: inspect(req.query),
-      body: inspect(req.body),
+      query: inspect(req.query, { depth: 4 }),
+      body: inspect(req.body, { depth: 4 }),
       ip: req.ip,
       ips: req.ips,
+      userAgent: req.headers['user-agent'],
     };
 
-    this.logger.log(logMessage);
+    this.logger.log(JSON.stringify(logMessage).replaceAll('\n', ''));
     next();
   }
 }
