744647d39ddabb19c8a467596384ea57f89ed4a8
ocknamo <s14pes@gmail.com>
2022-09-14 04:52:51 +0900

    fix: remove unneed try catch

--- src/worker/class/job.ts
+++ src/worker/class/job.ts
@@ -34,84 +34,79 @@ export class Job {
     }
     const connect = await this.connection;
 
-    try {
-      const result = await connect.query(
-        'select max(txIndex) as lastTxIndex from card',
-      );
+    const result = await connect.query(
+      'select max(txIndex) as lastTxIndex from card',
+    );
 
-      const { lastTxIndex } = result[0];
+    const { lastTxIndex } = result[0];
 
-      this.logger.log(`[readNewMonacard] lastTxIndex is ${lastTxIndex}`);
+    this.logger.log(`[readNewMonacard] lastTxIndex is ${lastTxIndex}`);
 
-      const counterpartyApiUrl = process.env.COUNTERPARTY_API_URL;
+    const counterpartyApiUrl = process.env.COUNTERPARTY_API_URL;
 
-      const api =
-        this.cpApi || new CounterpartyClientService(counterpartyApiUrl);
-      const issuances = await api.getIssuancesTxIndex(lastTxIndex ?? 0);
+    const api = this.cpApi || new CounterpartyClientService(counterpartyApiUrl);
+    const issuances = await api.getIssuancesTxIndex(lastTxIndex ?? 0);
 
-      const cards = issuances
-        .map((i) => {
-          const desc = parseDescription(i.description);
+    const cards = issuances
+      .map((i) => {
+        const desc = parseDescription(i.description);
 
-          if (2000 < (desc?.addDescription.length || 0)) {
-            this.logger.log(
-              `[readNewMonacard] Over 2000 description. asset: `,
+        if (2000 < (desc?.addDescription.length || 0)) {
+          this.logger.log(
+            `[readNewMonacard] Over 2000 description. asset: `,
+            i.asset,
+          );
+        }
+
+        return desc
+          ? new Card(
               i.asset,
-            );
-          }
+              i.asset_longname ?? null,
+              i.asset_group ?? null,
+              desc.cardName,
+              i.issuer,
+              '',
+              desc.addDescription,
+              i.status,
+              desc.tag,
+              desc.cid,
+              desc.ver,
+              i.tx_hash,
+              i.tx_index,
+            )
+          : undefined;
+      })
+      .filter((v) => v instanceof Card) as Card[];
 
-          return desc
-            ? new Card(
-                i.asset,
-                i.asset_longname ?? null,
-                i.asset_group ?? null,
-                desc.cardName,
-                i.issuer,
-                '',
-                desc.addDescription,
-                i.status,
-                desc.tag,
-                desc.cid,
-                desc.ver,
-                i.tx_hash,
-                i.tx_index,
-              )
-            : undefined;
-        })
-        .filter((v) => v instanceof Card) as Card[];
-
-      /**
-       * Insert when entity has new `id` or `asset`.
-       * In otherwise update entity.
-       */
-      await connect
-        .createQueryBuilder()
-        .insert()
-        .into(Card)
-        .values(cards)
-        .orUpdate(
-          [
-            'assetLongname',
-            'assetGroup',
-            'name',
-            'issuer',
-            'imgur',
-            'description',
-            'status',
-            'tag',
-            'cid',
-            'ver',
-            'txHash',
-            'txIndex',
-            'registTime',
-            'updateTime',
-          ],
-          ['id', 'asset'],
-        )
-        .execute();
-    } catch (error) {
-      throw error;
-    }
+    /**
+     * Insert when entity has new `id` or `asset`.
+     * In otherwise update entity.
+     */
+    await connect
+      .createQueryBuilder()
+      .insert()
+      .into(Card)
+      .values(cards)
+      .orUpdate(
+        [
+          'assetLongname',
+          'assetGroup',
+          'name',
+          'issuer',
+          'imgur',
+          'description',
+          'status',
+          'tag',
+          'cid',
+          'ver',
+          'txHash',
+          'txIndex',
+          'registTime',
+          'updateTime',
+        ],
+        ['id', 'asset'],
+      )
+      .execute();
   }
 
   /**
